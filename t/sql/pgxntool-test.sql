24d80ac28e05c45810d0dbe858391a61da97c23c
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-11-03 20:57:53 -0500

    Fix for 9.1

--- t/sql/pgxntool-test.sql
+++ t/sql/pgxntool-test.sql
@@ -2,7 +2,7 @@ CREATE FUNCTION "pgxntool-test"(
   a int
   , b int
 ) RETURNS int LANGUAGE sql IMMUTABLE AS $body$
-SELECT a + b
+SELECT $1 + $2 -- 9.1 doesn't support named sql language parameters
 $body$;
 
 -- vi: expandtab ts=2 sw=2
